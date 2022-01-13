7238b5f034ed7ec7504a212c1f32b6084fff11fe
seongtaekkim <chxortnl@gmail.com>
2022-01-14 05:22:03 +0900

    hashMap (valid anagram, word pattern)

--- src/sectioin1/Hash.java
+++ src/sectioin1/Hash.java
@@ -1,5 +1,7 @@
 package sectioin1;
 
+import javax.swing.plaf.basic.BasicMenuUI;
+import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.Map;
@@ -10,7 +12,12 @@ public class Hash {
        // System.out.println(twosum(new int[]{13, 7, 5, 1, 3, 2}, 10));;
         //System.out.println(findFirstUniqueCharacter("nocodeprogram"));
 
-        System.out.println(somorphicStrings("aaaccd","xxweea"));
+        //System.out.println(somorphicStrings("aaaccd","xxweea"));
+
+        //System.out.println(validAnagram("nocodeprogram","promodernacog"));
+
+        System.out.println(wordPattern("acc", "kiwi grape apple"));
+        System.out.println(wordPattern("abba", "banana apple apple banana"));
     }
 
     /**
@@ -39,7 +46,7 @@ public class Hash {
 
     /**
      * find first unique character
-     *
+     * O(n)
      */
     public static int findFirstUniqueCharacter(String data) {
         char[] arr = data.toCharArray();
@@ -70,7 +77,7 @@ public class Hash {
 
     /**
      * somorpic Strings
-     *
+     * O(n)
      * @param str1
      * @param str2
      * @return
@@ -93,6 +100,70 @@ public class Hash {
             }
         }
         return true;
+    }
+
+    /**
+     * valid anagram
+     * O(n)
+     * @return
+     */
+    public static boolean validAnagram(String arr1, String arr2) {
+        HashMap<Character, Integer> hashMap = new HashMap<Character, Integer>();
+
+        if(arr1.length() != arr2.length()) {
+            return false;
+        }
+
+        char[] charArr1 = arr1.toCharArray();
+        char[] charArr2 = arr2.toCharArray();
+
+        for(int i=0; i < charArr1.length ; i++ ) {
+            if(hashMap.get(charArr1[i]) == null)
+                hashMap.put(charArr1[i], 1);
+            else {
+                int idx = hashMap.get(charArr1[i]);
+                hashMap.replace(charArr1[i], ++idx);
+            }
+        }
+
+        for(int i=0 ; i<charArr2.length ; i++) {
+            if(hashMap.get(charArr2[i]) < 1)
+                return false;
+            else {
+                int idx = hashMap.get(charArr2[i]);
+                hashMap.replace(charArr2[i], --idx);
+            }
+        }
+        return true;
+    }
+
+    /**
+     * word pattern
+     * O(n)
+     * @param str1
+     * @param str2
+     */
+    public static boolean wordPattern(String str1, String str2) {
+        char[] charArr = str1.toCharArray();
+        String[] strArr = str2.split(" ");
+
+        if(charArr.length != strArr.length ) {
+            return false;
+        }
+        HashMap<Character, String> hashMap = new HashMap<>();
+
+        for(int i=0 ; i<strArr.length ; i++) {
+            if(hashMap.get(charArr[i]) == null){
+                hashMap.put(charArr[i],strArr[i]);
+            } else {
+                if(hashMap.get(charArr[i]).equals(strArr[i])) {
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+
     }
 }
 
