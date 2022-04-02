567023587086e076466f997bb726f838f62f4c9f
seongtaekkim <chxortnl@gmail.com>
2022-04-02 18:18:53 +0900

    loop sum

---
+++ 003_loop/02_loopSum.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+ int T=0;
+ scanf("%d",&T);
+ int* A = (int*)malloc(sizeof(int)*T);
+ int* B = (int*)malloc(sizeof(int)*T);
+ int i=0;
+ while(i<T) {
+  scanf("%d %d",&A[i],&B[i]);
+  i++;
+ }
+ i=0;
+ while(i<T) {
+  printf("%d\n",A[i]+B[i]);
+  i++;
+ } 
+}
