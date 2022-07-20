158816432c306f715971f8ac1cf7c97dce09d3e4
seongtaekkim <chxortnl@gmail.com>
2022-07-20 19:39:28 +0900

    update

--- stack/max_stack.c
+++ stack/max_stack.c
@@ -5,8 +5,8 @@ typedef struct s_node
 {
 	struct s_node	*next;
 	struct s_node	*prev;
-	void		*data;
-	int		max_idx;
+	void			*data;
+	int				max_idx;
 }	t_node;
 
 void	push(t_node *top, t_node *bottom, void *data)
@@ -57,24 +57,33 @@ void	show(t_node *top, t_node *bottom)
 	
 	while (node != NULL && node != bottom)
 	{
-		printf("%c ",*(char *)node->data);
+		printf("%d ",*(int *)node->data);
 		node = node->next;
 	}
 	printf("\n");
 }
 
 
-
+/*
+ * stack 기능구현
+ * 1. push, pop, top
+ * 2. max - n(1)
+ */
 int	main(void)
 {
 	t_node top;
 	t_node bottom;
 	t_node max_top;
 	t_node max_bottom;
+	int	index;
 	int	input[] = {1,3,3,2,2,5,1};
 
-	push(&top, &bottom, &input);
+	index = 0;
+	while (index < 7)
+	{
+		push(&top, &bottom, &input[index]);
+		index++;
+	}
 	show(&top, &bottom);
-		
-
+	return (0);
 }
