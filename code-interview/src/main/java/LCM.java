ce47f6083cbc9537ab72c7ddf4794d55023af970
seongtaekkim <chxortnl@gmail.com>
2023-08-13 23:30:13 +0900

    new

---
+++ code-interview/src/main/java/LCM.java
@@ -0,0 +1,37 @@
+/**
+ * 최대공약수와 최소공배수 로직
+ */
+public class LCM {
+
+    public static int gcd(int[] arr) {
+        int result = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            result = gcd(result, arr[i]);
+        }
+        return result;
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    public static int lcm(int[] arr) {
+        int result = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            result = lcm(result, arr[i]);
+        }
+        return result;
+    }
+
+    public static int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(lcm(new int[]{1,2,3}));
+        System.out.println(gcd(12,18));
+        System.out.println(lcm(12,18));
+    }
+
+}
