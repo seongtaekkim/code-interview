86fc29b72955abaef229b0630d2744529ea80dcc
seongtaekkim <chxortnl@gmail.com>
2023-11-12 10:12:31 +0900

    Test: ADD 10430 나머지

---
+++ baekjoon-practice/src/_202311/_10430.java
@@ -0,0 +1,16 @@
+package _202311;
+
+import java.util.Scanner;
+
+/**
+ * 10430 나머지
+ * https://www.acmicpc.net/problem/10430
+ */
+public class _10430 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        System.out.println((double) A / B);
+    }
+}
