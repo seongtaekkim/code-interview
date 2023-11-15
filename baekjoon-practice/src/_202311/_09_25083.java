4528b8675932c384a1215cb0298668206b0b9a1c
seongtaekkim <chxortnl@gmail.com>
2023-11-15 09:40:37 +0900

    Test: 25083 새싹

---
+++ baekjoon-practice/src/_202311/_09_25083.java
@@ -0,0 +1,16 @@
+package _202311;
+
+/**
+ * 25083 새싹
+ * https://www.acmicpc.net/problem/25083
+ */
+public class _09_25083 {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7\n"
+        +"r`-_   ,'  ,/\n"
+        +" \\. \". L_r'\n"
+        +"   `~\\/\n"
+        +"      |\n"
+        +"      |\n");
+    }
+}
