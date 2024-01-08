b0459980f729dcc9c8e320158c33ee92fa786056
seongtaekkim <chxortnl@gmail.com>
2024-01-08 16:34:39 +0900

    Test: ADD 1753 최단경로

---
+++ baekjoon-practice/src/graph/_1753.java
@@ -0,0 +1,77 @@
+package graph;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+/**
+ * 1753 최단경로
+ * https://www.acmicpc.net/problem/1753
+ */
+public class _1753 {
+    static int V, E, start;
+    static ArrayList<ArrayList<Node>> graph;
+
+    static class Node {
+        int idx, cost;
+
+        Node(int idx, int cost) {
+            this.idx = idx;
+            this.cost = cost;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        // 초기화
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        V = Integer.parseInt(st.nextToken()); // 노드개수
+        E = Integer.parseInt(st.nextToken()); // 간선개수
+        start = Integer.parseInt(br.readLine());
+        graph = new ArrayList<ArrayList<Node>>();
+        for (int i = 0; i < V + 1; i++) {
+            graph.add(new ArrayList<Node>());
+        }
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            graph.get(s).add(new Node(e, c));
+        }
+
+        int[] dist = new int[V + 1];
+        for (int i = 0; i < V + 1; i++) {
+            dist[i] = Integer.MAX_VALUE;
+        }
+
+        PriorityQueue<Node> q = new PriorityQueue<Node>((o1, o2) -> Integer.compare(o1.cost, o2.cost));
+        q.offer(new Node(start, 0));
+        dist[start] = 0;
+        while (!q.isEmpty()) {
+            Node curNode = q.poll();
+
+            if (dist[curNode.idx] < curNode.cost) {
+                continue;
+            }
+
+            for (int i = 0; i < graph.get(curNode.idx).size(); i++) {
+                Node nxtNode = graph.get(curNode.idx).get(i);
+                if (dist[nxtNode.idx] > curNode.cost + nxtNode.cost) {
+                    dist[nxtNode.idx] = curNode.cost + nxtNode.cost;
+                    q.offer(new Node(nxtNode.idx, dist[nxtNode.idx]));
+                }
+            }
+        }
+
+        for (int i=1 ; i< dist.length ; i++) {
+            if (dist[i] == 2147483647)
+                System.out.println("INF");
+            else
+                System.out.println(dist[i]);
+        }
+    }
+}
